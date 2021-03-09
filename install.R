6cfcd89e5a53add05b7cc7c783fcac4b0d0aa8db
Michaël Bekaert <michael.bekaert@stir.ac.uk>
2021-03-09 12:41:52 +0000

    Initial commit

---
+++ install.R
@@ -0,0 +1,10 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager");
+BiocManager::install("SNPassoc");
+install.packages("poppr", quietly = TRUE);
+install.packages("vcfR", quietly = TRUE);
+install.packages("ape", quietly = TRUE);
+install.packages("adegenet", quietly = TRUE);
+install.packages("RColorBrewer", quietly = TRUE);
+install.packages("tidyr", quietly = TRUE);
+install.packages("ggplot2", quietly = TRUE);
