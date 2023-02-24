3712dc090e39e35d16a0303424ae2f3b8acff8c2
Michaël Bekaert <michael.bekaert@stir.ac.uk>
2023-02-24 14:09:18 +0000

    Update binder.org limitations

--- install.R
+++ install.R
@@ -1,3 +1,12 @@
+library(devtools)
+install.packages('BiocManager')
+BiocManager::install('VariantAnnotation')
+BiocManager::install('BiocStyle')
+BiocManager::install('TxDb.Hsapiens.UCSC.hg19.knownGene')
+BiocManager::install('org.Hs.eg.db')
+install.packages("SNPassoc")
+
+install.packages("adegenet");
 install.packages("units");
 install.packages("ape");
 install.packages("poppr");
@@ -7,4 +16,4 @@ install.packages("tidyr");
 install.packages("ggplot2");
 install.packages("C50")
 install.packages("caret")
-install.packages("SNPassoc");
+
